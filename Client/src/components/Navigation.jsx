752ff8c5730345f5137477ba88e550e200c5b86a
Stefan Antenev <steffann90@abv.bg>
2024-07-28 14:50:26 +0300

    Add dynamic navbar

--- Client/src/components/Navigation.jsx
+++ Client/src/components/Navigation.jsx
@@ -1,54 +1,34 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
 
 export default function Navigation() {
-    const isLoggedIn = localStorage.length > 0;
+    const { isAuthenticated } = useContext(AuthContext);
 
-    
-    if (isLoggedIn) {
-        const handleLogout = () => {
-            localStorage.clear();
-            // Add any additional logout logic here
-        };
 
-        return (
-            <nav>
-                <ul>
-                    <li><Link className="navbutton" to="/" >Home</Link></li>
-                    <li><Link className="navbutton" to="/recipies" >Recipies</Link></li>
-                    <li><Link className="navbutton" to="/create" >Create</Link></li>
-                    <li><Link className="navbutton" to="/search" >Search</Link></li>
-                    <li><Link className="navbutton" to="/contacts" >Contact</Link></li>
-                    <li><Link className="navbutton" to="/about" >About</Link></li>
-                    <li><Link className="navbutton" to="/register" onClick={handleLogout}>Logout</Link></li>
-                    <li><Link className="profilebutton" to="/profile" >Profile</Link></li>
-                </ul>
-            </nav>
-        );
-    } else {
-        return (
-            <nav>
-                <ul>
-                    <li><Link className="navbutton" to="/" >Home</Link></li>
-                    <li><Link className="navbutton" to="/login" >Login</Link></li>
-                    <li><Link className="navbutton" to="/register" >Register</Link></li>
-                    <li><Link className="navbutton" to="/contacts" >Contact</Link></li>
-                    <li><Link className="navbutton" to="/about" >About</Link></li>
-                </ul>
-            </nav>
-        );
-    }
-    // return (
-    //     <nav>
-    //         <ul>
-    //             <li><a className="navbutton" href="/" >Home</a></li>
-    //             <li><a className="navbutton" href="/recipies" >Recipies</a></li>
-    //             <li><a className="navbutton" href="/contacts" >Contact</a></li>
-    //             <li><a className="navbutton" href="/about" >About</a></li>
-    //             <li><a className="navbutton" href="/login" >Login</a></li>
-    //             <li><a className="navbutton" href="/register" >Register</a></li>
-    //             <li><a className="navbutton" href="/register" >Logout</a></li>
-    //             <li><a className="navbutton" href="/create" >Create</a></li>
-    //         </ul>
-    //     </nav>
-    // );
+    return (
+        <nav>
+            {isAuthenticated
+                ? (
+                    <ul>
+                        <li><Link className="navbutton" to="/" >Home</Link></li>
+                        <li><Link className="navbutton" to="/recipies" >Recipies</Link></li>
+                        <li><Link className="navbutton" to="/create" >Create</Link></li>
+                        <li><Link className="navbutton" to="/search" >Search</Link></li>
+                        <li><Link className="navbutton" to="/contacts" >Contact</Link></li>
+                        <li><Link className="navbutton" to="/about" >About</Link></li>
+                        <li><Link className="navbutton" to="/register" >Logout</Link></li>
+                        <li><Link className="profilebutton" to="/profile" >Profile</Link></li>
+                    </ul>
+                ) : (
+                    <ul>
+                        <li><Link className="navbutton" to="/" >Home</Link></li>
+                        <li><Link className="navbutton" to="/login" >Login</Link></li>
+                        <li><Link className="navbutton" to="/register" >Register</Link></li>
+                        <li><Link className="navbutton" to="/contacts" >Contact</Link></li>
+                        <li><Link className="navbutton" to="/about" >About</Link></li>
+                    </ul>
+                )}
+        </nav>
+    );
 }
